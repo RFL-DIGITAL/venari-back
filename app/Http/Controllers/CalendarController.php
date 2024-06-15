39308799524f51c9a7088b44c485f338ed89c412
SuperTapok <puhovskijsa@kuzstu.ru>
2024-06-16 04:21:45 +0700

    feat: :sparkles: Добавил метод получения доступных слотов для виджета в чате.

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Google;
 use App\Models\Calendar;
 use App\Services\CalendarService;
+use DateTime;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -45,6 +46,25 @@ class CalendarController extends Controller
     {
         return $this->successResponse(
             ['calendar_id' => $this->calendarService->getCalendarID($request->user()->id)]
+        );
+    }
+
+    public function getAvailableSlotsInMonth(Request $request): JsonResponse
+    {
+        return $this->successResponse(
+            $this->calendarService->getAvailableSlotsInMonth(
+                $request->accountable_id,
+                $request->month
+            )
+        );
+    }
+
+    public function bookSlot(Request $request): JsonResponse
+    {
+        return $this->successResponse(
+            $this->calendarService->bookSlot(
+                $request->event_id,
+            )
         );
     }
 
