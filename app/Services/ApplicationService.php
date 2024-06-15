02227cadff7702687b6dcac69b279fcae86f0d4e
KupryashinStepan <kupryashin.stepan@yandex.ru>
2024-06-15 15:04:24 +0700

    fix: кондидаты

--- app/Services/ApplicationService.php
+++ app/Services/ApplicationService.php
@@ -59,39 +59,36 @@ class ApplicationService {
         ?int $lower_salary
     )
     {
-        $experience_id = $experience_id != null ? $experience_id : 1;
 
-
-        $usersBuilder = User::with(['resumes']);
-
+        $usersBuilder = User::with('resumes');
         if($experience_id) {
-            $usersBuilder-with(['resumes'])->whereRelation('resumes', 'experience_id', '=', $experience_id);
+            $usersBuilder->whereRelation('resumes', 'experience_id', '=', $experience_id);
         }
 
         if ($city_id != null) {
-            $usersBuilder->with(['city'])->whereRelation('city', 'id', $city_id);
+            $usersBuilder->whereRelation('city', 'id', $city_id);
         }
 
         if ($specialization_id != null) {
-            $usersBuilder->with(['resumes'])->whereRelation('resume', 'specialization_id', $specialization_id);
+            $usersBuilder->whereRelation('resume', 'specialization_id', $specialization_id);
         }
 
         if ($employment_id != null) {
-            $usersBuilder->with(['resumes'])->whereRelation('resume', 'employment_id', $employment_id);
+            $usersBuilder->whereRelation('resume', 'employment_id', $employment_id);
         }
 
         if ($program_type_id != null) {
-            $usersBuilder->with(['resumes'])
+            $usersBuilder
             ->whereRelation('resume.resumeProgramSchools.programSchool', 'program_type_id', $program_type_id);
         }
 
         if ($higher_salary != null) {
-            $usersBuilder->with(['resumes'])
+            $usersBuilder
             ->whereRelation('resume', (int)'salary', '<' ,$higher_salary);
         }
 
         if ($lower_salary != null) {
-            $usersBuilder->with(['resumes'])
+            $usersBuilder
             ->whereRelation('resume', (int)'salary', '<' ,$lower_salary);
         }
 
