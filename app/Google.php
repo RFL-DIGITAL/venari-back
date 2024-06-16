f1c1b0ecc3051d3f668e5fc52451323551a4b30d
SuperTapok <puhovskijsa@kuzstu.ru>
2024-06-17 03:25:03 +0700

    feat: :construction: WIP

--- app/Google.php
+++ app/Google.php
@@ -15,7 +15,7 @@ class Google
         $client->setAuthConfig($this->SECRETS_PATH);
         $client->setClientId('789109854481-se2ttlco5kh487ne4vcjkhv9rhpogj3a.apps.googleusercontent.com');
         $client->setClientSecret('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');
-        $client->setRedirectUri('http://localhost:5173/hr/calendar');
+        $client->setRedirectUri('https://venari.rfld.ru/hr/calendar');
         $client->setScopes(explode(',', 'https://www.googleapis.com/auth/calendar.app.created,https://www.googleapis.com/auth/calendar'));
         $client->setApprovalPrompt('force');
         $client->setAccessType('offline');
